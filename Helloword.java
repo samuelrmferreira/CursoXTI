06bfcd231a28e56bc8479f3c0ac4f9db038442a9
samuelrmferreira <samuelrmferreira@gmail.com>
2019-09-28 10:20:01 -0300

    Primeiro commit do curso de java

---
+++ Helloword.java
@@ -0,0 +1,10 @@
+/**
+ * Programa que imprime texto na tela.
+ * @autor Samuel Ferreira
+ * data: 28/09/2019
+ */
+public class Helloword {
+	public static void main (String[] args){
+		System.out.print("Oi mundo!");
+	}
+}
